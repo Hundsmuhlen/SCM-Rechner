9bde9e50136f1f5b05067161e5d0da053764edf1
Hundsmuhlen <61830815+Hundsmuhlen@users.noreply.github.com>
2021-03-20 19:14:50 +0100

    am dual ascent gearbeitet

--- diskreteVerfahren.py
+++ diskreteVerfahren.py
@@ -11,14 +11,14 @@ def dual_ascent():
         [12, 8, 4, 0, 6]
     ]
 
-    F = [12,6,9,9,12] # Kosten für Eröffnung
+    F = [12, 6, 9, 9, 12]  # Kosten für Eröffnung
     costMatrix = input_array
     sortedCostMatrix = np.asarray(np.sort(np.array(costMatrix)))
     print(sortedCostMatrix)
 
-    I = [i+1 for i in range(len(costMatrix))]
+    I = [i + 1 for i in range(len(costMatrix))]
     vi = [[i[0] for i in sortedCostMatrix]]
-    Ji = [[[row.index(i)+1] for row, i in zip(input_array, vi[0])]]
+    Ji = [[[row.index(i) + 1] for row, i in zip(input_array, vi[0])]]
     print(I)
     print(vi)
     print(f"Ji = {Ji}")
@@ -28,70 +28,94 @@ def dual_ascent():
 
     print(sj)
     print(k)
-    #------------ Initialization done
+    # ------------ Initialization done
+    print("----------- Initialisierung abgeschlossen ----------")
 
     while True:
-        iteration: int = 1 # Da initialisierung bereits durchgeführt wurde
+        iteration: int = 1  # Da initialisierung bereits durchgeführt wurde
         vi.append([])
         Ji.append([])
 
         for inde, i in enumerate(I):
-            indizes = Ji[-2][i-1]
-
-
-            delta_i = min([sj[-1][indi - 1] for indi in indizes])
-            if delta_i == 0:
-                print(f"Delta{i} = 0")
-                I.pop(inde)
-                if len(I) == 0:
+            if i == 0:
+                # todo platzhalter Werte eintragen
+                if all([ii == 0 for ii in I]):
                     break
-                Ji[-1][-1] = Ji[-2][i - 1]
-                vi[iteration].append(vi[iteration - 1])
-                sj.append(sj[-1])
+
                 continue
-            print(f"Delta{i} = {delta_i}")
-            delta = min(delta_i, sortedCostMatrix[i-1][k[i-1]] - vi[iteration-1][i-1])
+
+            indizes = Ji[-2][i - 1]
+
+            # 1. Bestimme delta i - das ist da niedrigste sj für j in J
+            # Ist delta i null, entferne i aus I
+            delta_i = min([sj[-1][indi - 1] for indi in indizes])
+
+            if delta_i == 0:
+                print(f"Delta-i{i} = 0")
+                I[inde] = 0
+                Ji[-1].append(Ji[-2][i - 1])
+                vi[iteration].append(vi[iteration - 1][i - 1])
+                sj.append(sj[-1])
+                # todo macht die reihenfolge sinn?
+
+                if all([ii == 0 for ii in I]):
+                    break
+                continue
+
+
+            # Minimiere delta_i oder das k-te Kostenelement des aktuellen Kunden minus
+            # sein aktuelles vi. gewinnt, das delta_i, Streiche den Kunden i aus I
+            delta = min(delta_i,
+                        sortedCostMatrix[i - 1][k[i - 1] - 1] - vi[- 2][i - 1]
+                        )
+
+            print(f"Delta = {delta}")
+
             if delta == delta_i:
                 print("Deltas gleich groß")
-                I.pop(inde)
-                if len(I) == 0:
-                    break
-                Ji[-1][-1] = Ji[-2][i - 1]
-                vi[iteration].append(vi[iteration - 1])
+                I[inde] = 0
+                Ji[-1].append(Ji[-2][i - 1])
+                vi[iteration].append(vi[iteration - 1][i - 1])
                 sj.append(sj[-1])
-                continue
-            ji_neu = costMatrix[i-1].index(sortedCostMatrix[i-1][k[i-1]])
-            k[i - 1] += 1
-            vi[iteration].append(vi[iteration-1] + delta)
+                # todo macht die reihenfolge sinn?
 
-            sj_temp = sj[-1]
+                if all([ii == 0 for ii in I]):
+                    break
+                continue
+
+            ji_neu: int = costMatrix[i - 1].index(sortedCostMatrix[i - 1][k[i - 1] - 1])
+            k[i - 1] += 1
+            vi[- 1].append(vi[- 2][i - 1] + delta)
+
+            sj_temp = sj[-1].copy()
 
             for indi in indizes:
-                sj_temp[indi - 1] += delta
+                sj_temp[indi - 1] = sj_temp[indi - 1] - delta
 
             sj.append(sj_temp)
 
-            Ji[-1].append(Ji[-2][i-1])
+            #todo die jis passen noch nicht so recht!!!
+            Ji[-1].append(Ji[-2][i - 1])
             Ji[-1][-1].append(ji_neu)
 
         print(f"sj = {sj}")
         print(f"vi{iteration} = {vi}")
         print(f"Ji{iteration} = {Ji}")
 
-
         break
+
 
 def greedy():
     input_array = [
-        [0, 3,10,4,10,5],
-        [8,0,1,10,7,6],
-        [5,7,0,3,3,10],
-        [2,2,5,0,2,9],
-        [6,10,3,9,0,5],
-        [4,1,2,1,9,0]
+        [0, 3, 10, 4, 10, 5],
+        [8, 0, 1, 10, 7, 6],
+        [5, 7, 0, 3, 3, 10],
+        [2, 2, 5, 0, 2, 9],
+        [6, 10, 3, 9, 0, 5],
+        [4, 1, 2, 1, 9, 0]
     ]
 
-    fj = [8,4,5,5,3,8]  # Kosten für Eröffnung
+    fj = [8, 4, 5, 5, 3, 8]  # Kosten für Eröffnung
 
     costMatrix = input_array
     sortedCostMatrix = np.asarray(np.sort(np.array(costMatrix)))
@@ -116,7 +140,6 @@ def greedy():
             delta_temp.append(sum(delta_temp_temp))
         omega_temp = [delta_temp[i] - fj[i] for i in range(len(fj))]
 
-
         delta.append(delta_temp)
         omega.append(omega_temp)
         chosen_j = omega_temp.index(max(omega_temp))
@@ -134,13 +157,12 @@ def greedy():
             print("breaaak")
             break
 
-        print(f"Folgender Standort wird eröffnet {chosen_j+1}")
+        print(f"Folgender Standort wird eröffnet {chosen_j + 1}")
         print(f"Deltaj-{round_counter}: {delta_temp}")
         print(f"Omegaj-{round_counter}: {omega_temp}")
         print(f"ui-{round_counter}: {ui[-1]}")
         print(f"neue Kosten: {kosten}")
 
 
-
 if __name__ == "__main__":
-    greedy()
+    dual_ascent()
